0c597ae98ba6e4992b7beb69328130d3ed7cdcc0
Dylan Stocking <dstocking0823@gmail.com>
2024-11-16 15:55:17 -0500

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer">
+    <div style="width: 100%; text-align: center;">
+      <span>©{{ year }} All Rights Reserved. A side project by Dylan Stocking & Cole Weegar.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  border: 1px solid black;
+}
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <img alt="DevTides Logo" width="200px" src="../assets/dev_tides_logo.jpg"/>
+      <h1 style="margin-top: -3%;">
+        Dev Tides
+      </h1>
+      <span style="font-style: italic;">Ride the waves of developer news</span>
+    </div>
+    <MailchimpComponent/>
+    <div>
+      <h5>Dedicated Developer News – Curated by hand from a variety of trusted sources.</h5>
+    </div>
+  </div>
+  <div style="align-content: center; text-align: center">
+<!--    <Footer/>-->
+  </div>
+</template>
+
+<script>
+import MailchimpComponent from './MailchimpComponent.vue';
+import Footer from './Footer.vue';
+
+export default {
+  components: {
+    MailchimpComponent,
+    Footer
+  }
+}
+</script>
+
+<style scoped></style>
